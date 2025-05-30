206f66d13ada1dc634ce07a3ffc036b250b14d9c
Shirin <shirin.monzavi.1372@gmail.com>
2025-05-30 09:48:00 +0330

    Add project files.

---
+++ FacadeDesignPattern/Subsystems/DVDPlayer.cs
@@ -0,0 +1,10 @@
+﻿namespace FacadeDesignPattern.Subsystems;
+
+public class DVDPlayer
+{
+    public void On() =>
+        Console.WriteLine("DVD Player is ON");
+
+    public void Play(string movieName) =>
+        Console.WriteLine($"Playing movie {movieName}");
+}
